8a1bfaccb46f37c409e2f3c98b88ba86d662e353
Dhara <58555337+Dhara1978@users.noreply.github.com>
2022-12-15 12:54:44 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import { IonApp, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import { 
+  IonApp, 
+  IonContent, 
+  IonHeader, 
+  IonInput, 
+  IonItem, 
+  IonLabel,
+  IonList,
+  IonTitle, 
+  IonToolbar } from '@ionic/react';
+import { useState } from 'react'
+
+function getToday() {
+  return new Date().toISOString().slice(0, 'yyyy-mm-dd'.length)
+}
 
 function App() {
+const [birthDate, setBirthDate] = useState('');
+const [targetDate, setTargetDate] = useState(getToday);
   return (
     <IonApp>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>My App</IonTitle>
+          <IonTitle>Biorhythms</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <p>Add some content here…</p>
+        <IonList>
+          <IonItem>
+            <IonLabel position="floating">Date of Birth:</IonLabel>
+            <IonInput type="date" value={birthDate} 
+            onIonChange={(event) => setBirthDate(event.detail.value)} 
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Target Date:</IonLabel>
+            <IonInput type="date" value={targetDate} 
+            onIonChange={(event) => setTargetDate(event.detail.value)} 
+            />
+          </IonItem>
+        </IonList>
+        <p>Born on <b>{birthDate}</b>, target date is <b>{targetDate}</b></p>
       </IonContent>
     </IonApp>
   );
