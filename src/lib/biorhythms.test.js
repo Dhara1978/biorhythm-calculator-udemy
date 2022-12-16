8adf7cc9eb44f281dde00164114e75bfba108ed8
Dhara <58555337+Dhara1978@users.noreply.github.com>
2022-12-16 12:27:16 +0000

    Update biorhythms.test.js

--- src/lib/biorhythms.test.js
+++ src/lib/biorhythms.test.js
@@ -2,5 +2,15 @@ import { calculateBiorhythms } from "./biorhythms";
 
 it("calculates the physical biorhythm", () => {
     const biorhythms = calculateBiorhythms("1999-12-31", "2022_12_31");
-    expect(biorhythms.physical).toBe(0.9977);
+    expect(biorhythms.physical).toBeCloseTo(0.9977);
+});
+
+it("calculates the emotional biorhythm", () => {
+    const biorhythms = calculateBiorhythms("1999-12-31", "2022_12_31");
+    expect(biorhythms.emotional).toBeCloseTo(0.2225);
+});
+
+it("calculates the intellectual biorhythm", () => {
+    const biorhythms = calculateBiorhythms("1999-12-31", "2022_12_31");
+    expect(biorhythms.intellectual).toBeCloseTo(-0.4582);
 });
